ed1267f83740a7129b9dc38627f8027af3dc5fa9
BBDUYU <tmdwhddh@gmail.com>
2026-01-15 12:49:37 +0900

    mvc spring

--- SL01_MVC/src/main/java/org/doit/ik/ScottController.java
+++ SL01_MVC/src/main/java/org/doit/ik/ScottController.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.doit.ik.domain.DeptDTO;
 import org.doit.ik.domain.EmpDTO;
 import org.doit.ik.mapper.TimeMapper;
@@ -23,49 +25,60 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.Setter;
 
 @Controller
 public class ScottController {
-	
+
 	@Autowired
-	//@Setter(onMethod=@__({@Autowired}))
+	// @Setter(onMethod=@__({@Autowired}))
 	private DeptMapper deptMapper;
 	@Autowired
 	private EmpMapper empMapper;
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ScottController.class);
 
 //	@RequestMapping(value = "/scott/dept", method = RequestMethod.GET)
-	@GetMapping(value="/scott/dept")
+	@GetMapping(value = "/scott/dept")
 	public String dept(Locale locale, Model model) {
 		logger.info("> ScottContoller.dept()");
 		ArrayList<DeptDTO> list = this.deptMapper.selectDept();
-		
-		model.addAttribute("list", list );
-		
+
+		model.addAttribute("list", list);
+
 		return "/scott/dept";
 	}
+
+	// 모든 사원정보 처리 컨트롤러 메서드
 	
-	//모든 사원정보 처리 컨트롤러 메서드
-	/*
-	 * @PostMapping("/scott/emp") public String emp(Locale locale, Model model) {
-	 * logger.info("> ScottContoller.emp()"); ArrayList<EmpDTO> list =
-	 * this.empMapper.selectEmp();
-	 * 
-	 * model.addAttribute("list", list );
-	 * 
-	 * return "/scott/emp"; }
-	 */
-	
-	@PostMapping("/scott/emp/{deptno}")
-	public ResponseEntity<List<EmpDTO>> selectEmp(@PathVariable("deptno")int deptno) {
-		logger.info("> ScottContoller.selectEmp()");
-		ArrayList<EmpDTO> list = this.empMapper.selectEmpByDeptno(deptno);
-				
-		return new ResponseEntity<>(list,HttpStatus.OK);
+	 @PostMapping("/scott/emp") public String emp(Locale locale, Model model,HttpServletRequest request,@RequestParam("deptno")int deptnoArr[]) {
+	 logger.info("> ScottContoller.emp()"); 
+	 /*
+	 int deptnoArr[]=null;
+	 String pDeptnoArr[] = request.getParameterValues("deptno");
+	 
+	 deptnoArr=new int[pDeptnoArr.length];
+	 for (int i = 0; i < pDeptnoArr.length; i++) {
+		deptnoArr[i] = Integer.parseInt(pDeptnoArr[i]);
 	}
+	*/ 
+//	 ArrayList<EmpDTO> list =this.empMapper.selectEmp();
+	 ArrayList<EmpDTO> list =this.empMapper.selectEmp(deptnoArr);
+	 
+	 model.addAttribute("list", list );
+	 
+	 return "/scott/emp"; }
 	
-	
+
+	/*
+	 * @PostMapping("/scott/emp/{deptno}") public ResponseEntity<List<EmpDTO>>
+	 * selectEmp(@PathVariable("deptno") int deptno) {
+	 * logger.info("> ScottContoller.selectEmp()"); ArrayList<EmpDTO> list =
+	 * this.empMapper.selectEmpByDeptno(deptno);
+	 * 
+	 * return new ResponseEntity<>(list, HttpStatus.OK); }
+	 */
+
 }
--- SL01_MVC/src/main/java/org/doit/ik/mapper/scott/EmpMapper.java
+++ SL01_MVC/src/main/java/org/doit/ik/mapper/scott/EmpMapper.java
@@ -9,6 +9,7 @@ import org.doit.ik.domain.EmpDTO;
 public interface EmpMapper {
 	// 모든 사원정보를 반환하는 메서드
 	ArrayList<EmpDTO> selectEmp();
-	ArrayList<EmpDTO> selectEmpByDeptno(int deptno);
+	// 특정 사원정보
+	ArrayList<EmpDTO> selectEmp(int deptnoArr[]);
 
 }
