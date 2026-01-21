3d49b499c825fd05ab64392b30dcae8783fc9498
BBDUYU <tmdwhddh@gmail.com>
2026-01-21 16:33:20 +0900

    Result Maps

---
+++ SL05_MYBATIS5/src/main/java/org/doit/ik/DeptEmpSalgradeController.java
@@ -0,0 +1,41 @@
+package org.doit.ik;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.doit.ik.domain.DeptEmpSalgradeDTO;
+import org.doit.ik.domain.EmpDTO;
+import org.doit.ik.mapper.DeptEmpSalgradeMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class DeptEmpSalgradeController {
+		
+	@Autowired
+	private DeptEmpSalgradeMapper deptEmpSalgradeMapper;	
+	
+	@GetMapping(value = "/dept/emp")
+	public void getDeptEmpSalgrade(Locale locale, Model model) {
+		log.info("@ @ @ DeptEmpSalgradeController.getDeptEmpSalgrade()...");
+		List<DeptEmpSalgradeDTO> deptList = this.deptEmpSalgradeMapper.getDept();
+		//forEach : 10,20,30,40
+		for(DeptEmpSalgradeDTO dto : deptList) {
+			List<EmpDTO> empList = this.deptEmpSalgradeMapper.getEmpOfDept(dto.getDeptno());
+			dto.setEmpList(empList);
+		}
+		model.addAttribute("desList",deptList);
+	}
+	
+}
