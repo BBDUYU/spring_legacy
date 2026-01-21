6154033be13fd24b4b95ba5c861694c831febdf6
BBDUYU <tmdwhddh@gmail.com>
2026-01-21 15:42:06 +0900

    mybatis-2

---
+++ SL05_MYBATIS2/src/main/java/org/doit/ik/domain/DeptDTO.java
@@ -0,0 +1,18 @@
+package org.doit.ik.domain;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Alias("DeptDTOAlias")
+public class DeptDTO {
+	private int deptno;
+	private String dname;
+	private String loc;
+	private int numberOfEmps;
+}
