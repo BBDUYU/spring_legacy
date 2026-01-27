070296cfee9dedfd369a399de7fe6e4d0e0aded7
BBDUYU <tmdwhddh@gmail.com>
2026-01-27 11:30:33 +0900

    isolation

---
+++ SL09_NLTRANSACTION5_ISOLATION/src/main/java/org/doit/ik/controller/JoinController.java
@@ -0,0 +1,110 @@
+package org.doit.ik.controller;
+
+import java.beans.PropertyEditorSupport;
+
+import org.doit.ik.domain.MemberVO;
+import org.doit.ik.persistence.MemberDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/joinus/*")
+public class JoinController {
+	
+	@Autowired
+	private MemberDao memberDao;
+	
+	// 회원가입
+	@GetMapping("/join.htm")
+	public String join() throws Exception{
+		return "join.jsp";
+	}
+	
+	
+	
+	// 가입
+	//[3] 336p
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(String.class, "habit",new PropertyEditorSupport() {
+			@Override
+			public void setAsText(String text) {
+				setValue(text);
+			
+			}
+			@Override
+			public void setValue(Object value) {
+				if(value instanceof String[]) {
+					String combinedHabit = String.join("/", (String[])value);
+					super.setValue(combinedHabit);
+				}else {
+					super.setValue(value);
+				}
+			
+		}
+		});
+	}
+	
+	@PostMapping("/join.htm") 
+	public String join(MemberVO memberVO
+			,@RequestParam("year") String year
+			,@RequestParam("month") String month
+			,@RequestParam("day") String day
+			,@RequestParam(value="habit") String []habitArr
+			) throws Exception{
+		String birth = String.format("%s-%s-%s",year,month,day);
+		memberVO.setBirth(birth);
+		
+		String combineHabit = String.join("/",habitArr);
+		memberVO.setHabit(combineHabit);
+		
+		this.memberDao.insert(memberVO);
+		return "redirect:../index.htm";
+	}
+	// [2]
+	/*
+	@PostMapping("/join.htm") 
+	public String join(MemberVO memberVO
+			,@RequestParam("year") String year
+			,@RequestParam("month") String month
+			,@RequestParam("day") String day
+			,@RequestParam(value="habit") String []habitArr
+			) throws Exception{
+		String birth = String.format("%s-%s-%s",year,month,day);
+		memberVO.setBirth(birth);
+		
+		String combineHabit = String.join("/",habitArr);
+		memberVO.setHabit(combineHabit);
+		
+		this.memberDao.insert(memberVO);
+		return "redirect:../index.htm";
+	}
+	*/
+	/* [1]
+	@PostMapping("/join.htm") 
+	public String join(MemberVO memberVO
+			,@RequestParam("year") String year
+			,@RequestParam("month") String month
+			,@RequestParam("day") String day
+			) throws Exception{
+		String birth = String.format("%s-%s-%s",year,month,day);
+		memberVO.setBirth(birth);
+		this.memberDao.insert(memberVO);
+		return "redirect:../index.htm";
+	}
+	*/
+	// 로그인
+	@GetMapping("/login.htm")
+	public String login() throws Exception{
+		return "login.jsp";
+	}
+}
---
+++ SL09_NLTRANSACTION5_ISOLATION/src/main/java/org/doit/ik/persistence/NoticeDaoImpl.java
@@ -0,0 +1,248 @@
+package org.doit.ik.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.doit.ik.domain.NoticeVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@Repository
+public class NoticeDaoImpl implements NoticeDao {
+
+	@Autowired
+	private NamedParameterJdbcTemplate npJdbcTemplate;
+
+	/*
+	@Autowired
+	private DataSourceTransactionManager transactionManager;
+	 */
+
+	@Autowired
+	private TransactionTemplate transactionTemplate;
+
+
+	// 1. 검색 조건에 맞는 공지사항의 갯수 반환하는 메서드 
+	public int getCount(String field, String query) throws ClassNotFoundException, SQLException
+	{
+		String sql = "SELECT COUNT(*) CNT "
+				+ "   FROM NOTICES "
+				+ "   WHERE "+field+" LIKE :query"; 
+
+		MapSqlParameterSource paramSource = new MapSqlParameterSource();
+		paramSource.addValue("query", "%"+query+"%");
+
+		return this.npJdbcTemplate.queryForObject(sql, paramSource, Integer.class);
+	}
+
+	// 2. 검색 조건에 맞는 공지사항 목록을 반환하는 메서드 
+	public List<NoticeVO> getNotices(
+			int page,     // 현재 페이지 번호 
+			String field, // 검색조건
+			String query  // 검색어
+			) throws ClassNotFoundException, SQLException
+	{               
+
+		int srow = 1 + (page-1)*15; // 1, 16, 31, 46, 61, ... an = a1 + (n-1)*d
+		int erow = 15 + (page-1)*15; //15, 30, 45, 60, 75, ...
+
+		String sql = "SELECT * "
+				+ "  FROM ( "
+				+ "                 SELECT ROWNUM NUM, N.* "
+				+ "                 FROM ("
+				+ "                          SELECT * "
+				+ "                          FROM NOTICES "
+				+ "                          WHERE "+field+" LIKE :query "
+				+ "                   ORDER BY REGDATE DESC"
+				+ "                ) N"
+				+ "  ) "
+				+  " WHERE NUM BETWEEN :srow AND :erow ";      
+
+		Map<String , Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("query", "%"+query+"%");
+		paramMap.put("srow", srow);
+		paramMap.put("erow", erow);
+
+
+		return this.npJdbcTemplate.query(
+				sql
+				, paramMap
+				, new BeanPropertyRowMapper<NoticeVO>(NoticeVO.class)
+				);
+	}
+
+	// 3. 공지사항 삭제 메서드 
+	public int delete(String seq) throws ClassNotFoundException, SQLException
+	{
+		String sql = "DELETE NOTICES "
+				+ "   WHERE SEQ = :seq";
+
+		MapSqlParameterSource paramSource = new MapSqlParameterSource();
+		paramSource.addValue("seq", seq);
+
+		return this.npJdbcTemplate.update(sql, paramSource);
+	}
+
+	// 4. 공지사항 수정 메서드 
+	public int update(
+			NoticeVO noticeVO   // 수정할 공지사항 내용이 들어있는 VO 객체
+			) throws ClassNotFoundException, SQLException{
+		// 
+		String sql = "UPDATE NOTICES "
+				+ "   SET TITLE = :title, CONTENT = :content, FILESRC = :filesrc "
+				+ "    WHERE SEQ = :seq "; 
+		SqlParameterSource paramSource =  new BeanPropertySqlParameterSource(noticeVO);
+
+
+		/*
+		 * MapSqlParameterSource paramSource = new MapSqlParameterSource();
+		 * paramSource.addValue("title", noticeVO.getTitle());
+		 * paramSource.addValue("content", noticeVO.getContent());
+		 * paramSource.addValue("filesrc", noticeVO.getFilesrc());
+		 * paramSource.addValue("seq", noticeVO.getSeq());
+		 */
+
+		return this.npJdbcTemplate.update(sql, paramSource);    
+	}
+
+	// 5. 해당 글번호의 공지사항을 반환하는 메서드: 공지사항 보기
+	public NoticeVO getNotice(String seq) throws ClassNotFoundException, SQLException
+	{
+		String sql = "SELECT * "
+				+ "   FROM NOTICES "
+				+ "   WHERE SEQ = :seq " ; 
+
+		MapSqlParameterSource paramSource = new MapSqlParameterSource();
+		paramSource.addValue("seq", seq);
+
+		return this.npJdbcTemplate.queryForObject(sql
+				, paramSource
+				, new BeanPropertyRowMapper<NoticeVO>(NoticeVO.class) );
+
+	}
+	// 6. 공지사항 쓰기 메서드 [2]
+//	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	@Transactional
+	public int insert(NoticeVO noticeVO) throws ClassNotFoundException, SQLException {
+		String sql1 = "INSERT INTO NOTICES "
+				+ " (SEQ, TITLE, CONTENT, WRITER, REGDATE, HIT, FILESRC) "
+				+ " VALUES"
+				+ "( "
+				+ "   (SELECT NVL(MAX(TO_NUMBER(SEQ))+1,1) FROM NOTICES), :title, :content, :writer, SYSDATE, 0, :filesrc"
+				+ ")"; 
+		String sql2 = "UPDATE MEMBER "
+				+ " SET point = point + 1 "
+				+ " WHERE ID = :id ";
+		// 515 p 참고
+
+		// A
+		SqlParameterSource paramSource =  new BeanPropertySqlParameterSource(noticeVO);
+		npJdbcTemplate.update(sql1, paramSource); 
+		// B
+		
+		SqlParameterSource paramSource2 = new MapSqlParameterSource("id", "user");
+		
+		return this.npJdbcTemplate.update(sql2, paramSource2); 
+	}
+	
+	// DBMS에서 지원하는 격리 수준의 기본 설정값
+	@Override
+	@Transactional(isolation = Isolation.DEFAULT)
+	public void hitUp(String seq) throws ClassNotFoundException, SQLException {
+		String sql = "UPDATE notices "
+				+" SET hit = hit + 1 "
+				+" WHERE seq = :seq ";
+		MapSqlParameterSource paramSource=new MapSqlParameterSource();
+		paramSource.addValue("seq", seq);
+		this.npJdbcTemplate.update(sql,paramSource);
+	}
+
+	@Override
+//	@Transactional(isolation = Isolation.SERIALIZABLE)
+	public int getHit(String seq) throws ClassNotFoundException, SQLException {
+		String sql = "SELECT hit "
+				+" FROM notices "
+				+" WHERE seq = :seq ";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("seq",seq);
+		
+		return this.npJdbcTemplate.queryForObject(sql, paramMap, Integer.class);
+	}
+	
+//	@Transactional(propagation = Propagation.REQUIRED)
+	/*
+	@Override
+	public void insertAndPointUpOfMember(NoticeVO noticeVO, String id) throws ClassNotFoundException, SQLException {
+		insert(noticeVO);
+		noticeVO.setTitle(noticeVO.getTitle()+"-02");
+		insert(noticeVO);
+	}
+	*/
+	// 6. 공지사항 쓰기 메서드 [1]
+	/*
+	public int insert(NoticeVO noticeVO) throws ClassNotFoundException, SQLException {
+
+		String sql = "INSERT INTO NOTICES "
+				+ " (SEQ, TITLE, CONTENT, WRITER, REGDATE, HIT, FILESRC) "
+				+ " VALUES"
+				+ "( "
+				+ "   (SELECT NVL(MAX(TO_NUMBER(SEQ))+1,1) FROM NOTICES), :title, :content, :writer, SYSDATE, 0, :filesrc"
+				+ ")"; 
+		SqlParameterSource paramSource =  new BeanPropertySqlParameterSource(noticeVO);
+
+
+		return this.npJdbcTemplate.update(sql, paramSource); 
+	}
+	*/
+	/*
+	@Transactional
+	@Override
+	public void insertAndPointUpOfMember(NoticeVO noticeVO, String id) throws ClassNotFoundException, SQLException {
+		String sql1 = "INSERT INTO NOTICES "
+				+ " (SEQ, TITLE, CONTENT, WRITER, REGDATE, HIT, FILESRC) "
+				+ " VALUES"
+				+ "( "
+				+ "   (SELECT NVL(MAX(TO_NUMBER(SEQ))+1,1) FROM NOTICES), :title, :content, :writer, SYSDATE, 0, :filesrc"
+				+ ")"; 
+		String sql2 = "UPDATE MEMBER "
+				+ " SET point = point + 1 "
+				+ " WHERE ID = :id ";
+		// 515 p 참고
+
+
+
+		// A
+		SqlParameterSource paramSource =  new BeanPropertySqlParameterSource(noticeVO);
+		npJdbcTemplate.update(sql1, paramSource); 
+		// B
+		SqlParameterSource paramSource2 = new MapSqlParameterSource("id", id);
+		npJdbcTemplate.update(sql2, paramSource2); 
+		}
+*/
+
+
+	
+}
